7273eb168efcae43d808808cdcedb7a9f57258a5
AngelMTR <angelmc.dev@gmail.com>
2025-02-21 16:04:43 +0330

    fix and add 'preact/compat'

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,18 @@
 import UnoCSS from 'unocss/vite'
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import preact from '@preact/preset-vite'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [
-      preact(),
-      UnoCSS(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [
+        preact(),
+        UnoCSS(),
+    ],
+    resolve: {
+        alias: {
+            'react': 'preact/compat',
+            'react-dom': 'preact/compat',
+            '@': path.resolve(__dirname, './src'),
+        }
     }
-  }
 })
