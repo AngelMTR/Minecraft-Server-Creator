4af01f69b00a61701f956641c81e3cfac56dd38e
AngelMTR <angelmc.dev@gmail.com>
2025-02-21 12:22:51 +0330

    add tailwind and some pages

---
+++ client/src/pages/Dashboard/index.jsx
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+
+const Dashboard = () => {
+    return (
+        <div>
+            Dashboard
+        </div>
+    )
+}
+
+export default Dashboard
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import preact from '@preact/preset-vite'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+      preact(),
+      tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
